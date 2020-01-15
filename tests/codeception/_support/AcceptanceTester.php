93c6b791180fad73319801ef4eb420713c56fa1d
craxrev <mortadha.ghanmi56@gmail.com>
2020-01-15 14:45:44 +0100

    add createSpace helper function

--- tests/codeception/_support/AcceptanceTester.php
+++ tests/codeception/_support/AcceptanceTester.php
@@ -34,4 +34,35 @@ class AcceptanceTester extends \AcceptanceTester
         return tests\codeception\_pages\MarketplacePage::openBy($this);
     }
 
+    public function createSpace($name, $description)
+    {
+        $this->amGoingTo('create a new xcoin space');
+        $this->click('#space-menu');
+        $this->waitForText('Create new space');
+        $this->click('Create new space');
+
+        $this->waitForText('Create new space', 30, '#globalModal');
+        $this->fillField('Space[name]', $name);
+        $this->fillField('Space[description]', $description);
+
+        $this->click('#access-settings-link');
+        $this->waitForElementVisible('.field-space-join_policy');
+
+        // Public visibility
+        $this->jsClick('#space-visibility [value="1"]');
+
+        // Everyone can enter
+        $this->jsClick('#space-join_policy [value="2"]');
+
+        $this->click('Next', '#globalModal');
+
+        $this->waitForText('Add Modules', 5, '#globalModal');
+        $this->click('Next', '#globalModal');
+
+        $this->waitForText('Invite members', 10, '#globalModal');
+        $this->click('Done', '#globalModal');
+        $this->waitForText($name);
+        $this->waitForText('This space is still empty!');
+    }
+
 }
