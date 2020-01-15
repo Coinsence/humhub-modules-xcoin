6f0bda3aedef8b00fa2ca98972cff84e3a3fbbc6
craxrev <mortadha.ghanmi56@gmail.com>
2020-01-15 14:47:04 +0100

    add test for adding crowdfunding project

--- tests/codeception/acceptance/CrowdfundingCest.php
+++ tests/codeception/acceptance/CrowdfundingCest.php
@@ -16,4 +16,55 @@ class CrowdfundingCest
 
     }
 
+    public function testAddingProject(AcceptanceTester $I)
+    {
+
+        $I->wantTo('ensure that adding crowfunding project works');
+
+        $spaceFrom = 'XcoinSpace 1';
+        $spaceTo = 'XcoinSpace 2';
+
+        $I->amUser1();
+        $I->createSpace($spaceFrom, 'XcoinSpaceDescription');
+        $I->createSpace($spaceTo, 'XcoinSpaceDescription');
+
+        $I->amOnCrowdfunding();
+        $I->see('Crowd Funding');
+        $I->click('Add Your Project');
+
+        $I->waitForText('Select from which space you want to create your campaign', 30, '#globalModal');
+        $I->executeJS('$("#funding-space_id").select2("open")');
+        $I->waitForElementVisible('.select2-container--open');
+        $I->see($spaceFrom, '.select2-container--open');
+        $I->see($spaceTo, '.select2-container--open');
+        $I->click($spaceFrom);
+        $I->click('Next');
+
+        $I->waitForText('Set funding request', 30, '#globalModal');
+        $I->executeJS('$("#funding-asset_id").select2("open")');
+        $I->waitForElementVisible('.select2-container--open');
+        $I->see($spaceTo, '.select2-container--open');
+        $I->click($spaceTo);
+        $I->click('Next');
+
+        $I->waitForText('Provide details', 30, '#globalModal');
+        $I->fillField('Funding[amount]', '20');
+        // $I->fillField('Funding[exchange_rate]', '1');
+        $I->fillField('Funding[title]', 'Testing Project');
+        $I->fillField('Funding[deadline]', '12/30/20');
+        $I->fillField('Funding[description]', 'ProjectDescription');
+        $I->executeJS('$("#funding-content .humhub-ui-richtext p").text("ProjectFullDescription")');
+        $I->click('Next');
+
+        $I->waitForText('Gallery', 30, '#globalModal');
+        $I->click('Save');
+
+
+
+
+
+
+
+    }
+
 }
