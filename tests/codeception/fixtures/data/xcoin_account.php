0d6d2eace12e2eb99bc9b506dcf4f7d881892b58
gdaly <daly.ghaith@gmail.com>
2020-01-17 02:45:16 +0100

    Unit testing : AccountHelperTest - 13.17%

--- tests/codeception/fixtures/data/xcoin_account.php
+++ tests/codeception/fixtures/data/xcoin_account.php
@@ -1,2 +1,9 @@
 <?php
-return [];
+return [
+    ['id' => 1, 'user_id' => null, 'space_id' => 1, 'title' => 'Space 1 Default Account', 'account_type' => 4, 'guid' => 's1e50e0d-82cd-41fc-8b0c-552392f5839a'],
+    ['id' => 2, 'user_id' => null, 'space_id' => 2, 'title' => 'Space 2 Default Account', 'account_type' => 4, 'guid' => 's1e50e0d-82cd-41fc-8b0c-552392f5839b'],
+    ['id' => 3, 'user_id' => null, 'space_id' => 3, 'title' => 'Space 3 Default Account', 'account_type' => 4, 'guid' => 's1e50e0d-82cd-41fc-8b0c-552392f5839c'],
+    ['id' => 4, 'user_id' => 1, 'space_id' => null, 'title' => 'User 1 Default Account', 'account_type' => 4, 'guid' => 'u1e50e0d-82cd-41fc-8b0c-552392f5839a'],
+    ['id' => 5, 'user_id' => 2, 'space_id' => null, 'title' => 'User 2 Default Account', 'account_type' => 4, 'guid' => 'u1e50e0d-82cd-41fc-8b0c-552392f5839b'],
+    ['id' => 6, 'user_id' => 3, 'space_id' => null, 'title' => 'User 3 Default Account', 'account_type' => 4, 'guid' => 'u1e50e0d-82cd-41fc-8b0c-552392f5839c'],
+];
