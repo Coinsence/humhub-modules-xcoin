78c842ac941335f3d589073d80fd0ce100d435d1
craxrev <mortadha.ghanmi56@gmail.com>
2020-01-31 15:54:03 +0100

    add initial travis ci config file and change testing server location

---
+++ .travis/setup-humhub.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# -e = exit when one command returns != 0, -v print each command before executing
+set -ev
+
+old=$(pwd)
+
+mkdir ${HUMHUB_PATH}
+cd ${HUMHUB_PATH}
+
+git clone --depth 1 https://github.com/Coinsence/humhub.git .
+composer install --prefer-dist --no-interaction
+
+npm install
+grunt build-assets
+
+cd ${HUMHUB_PATH}/protected/humhub/tests
+
+sed -i -e "s|'installed' => true,|'installed' => true,\n\t'moduleAutoloadPaths' => ['$(dirname $old)']|g" config/common.php
+cat config/common.php
+
+mysql -e 'CREATE DATABASE coinsence_test;'
+php codeception/bin/yii migrate/up --includeModuleMigrations=1 --interactive=0
+php codeception/bin/yii installer/auto
+php codeception/bin/yii search/rebuild
+
+php ${HUMHUB_PATH}/protected/vendor/bin/codecept build
