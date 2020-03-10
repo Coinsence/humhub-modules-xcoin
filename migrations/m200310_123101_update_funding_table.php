c9193d13b227576e5c12b9c775641b3af2f24165
gdalyy <daly.ghaith@gmail.com>
2020-03-10 14:00:00 +0100

    linking funding to challenge

---
+++ migrations/m200310_123101_update_funding_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200310_123101_update_funding_table
+ */
+class m200310_123101_update_funding_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('xcoin_funding','challenge_id', $this->integer());
+
+        $this->addForeignKey('fk_funding_challenge', 'xcoin_funding', 'challenge_id', 'xcoin_challenge', 'id', 'CASCADE', 'CASCADE');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200310_123101_update_funding_table cannot be reverted.\n";
+
+        return false;
+    }
+}
