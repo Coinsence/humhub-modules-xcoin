2f402e6ae5ea6aa44146d95082ff5e7b8101c4a5
ad <rafin_ala03@hotmail.fr>
2022-01-31 22:13:11 +0100

    fix error enabled crowdfunding for stats

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -33,25 +33,20 @@ class DashboardController extends Controller
         if (!parent::beforeAction($action)) {
             return false;
         }
-
-        if (!$this->module->isCrowdfundingEnabled()) {
-            throw new HttpException(403, Yii::t('XcoinModule.base', 'Crowdfunding is not enabled'));
-        }
-
         return true;
     }
 
 
-    public function actionStatistics($startDate = null , $endDate = null , $type = null)
+    public function actionStatistics($startDate = null, $endDate = null, $type = null)
     {
         return $this->render('statistics', [
-            'totalUsers'=>DashboardStatistics::getTotalUsers($startDate,$endDate,$type),
-            'totalTransactions'=>DashboardStatistics::getTotalOfTransactions($startDate,$endDate,$type),
-            'totalOffers'=>DashboardStatistics::getTotalOfMarketplaceOffers($startDate,$endDate,$type),
-            'totalMarketPlaces'=>DashboardStatistics::getTotalMarketplaces($startDate,$endDate,$type),
-            'startDate'=>$startDate,
-            'endDate'=>$endDate,
-            'type'=>$type
+            'totalUsers' => DashboardStatistics::getTotalUsers($startDate, $endDate, $type),
+            'totalTransactions' => DashboardStatistics::getTotalOfTransactions($startDate, $endDate, $type),
+            'totalOffers' => DashboardStatistics::getTotalOfMarketplaceOffers($startDate, $endDate, $type),
+            'totalMarketPlaces' => DashboardStatistics::getTotalMarketplaces($startDate, $endDate, $type),
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'type' => $type
         ]);
     }
 }
